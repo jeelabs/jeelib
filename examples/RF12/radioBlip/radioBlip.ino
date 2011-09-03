04a286671c14fef40f00ed5c998eb68a879fdf38
Jean-Claude Wippler <jc@wippler.nl>
2011-09-03 16:34:53 +0200

    restore previously lost low-power code

--- examples/RF12/radioBlip/radioBlip.ino
+++ examples/RF12/radioBlip/radioBlip.ino
@@ -1,32 +1,35 @@
 // Send out a radio packet every minute, consuming as little power as possible
 // 2010-08-29 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
 
-// all the low-power functions have been moved into the Ports library
-
 #include <JeeLib.h>
 
-static word payload;
+static long payload;
+
+// this must be added since we're using the watchdog for low-power waiting
+ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 
 void setup() {
-#ifndef SDCR
-    DDRB |= bit(0);
-    for (byte i = 0; i < 6; ++i) {
-        PINB = bit(0);
-        delay(128);
-    }
-#endif
     rf12_initialize(4, RF12_868MHZ, 5);
+    rf12_sleep(RF12_SLEEP);
+    
+    // stay off  3 minutes after reset, to make sure that power is stable
+    for (byte i = 0; i < 3; ++i)
+        Sleepy::loseSomeTime(60000);
 }
 
 void loop() {
     ++payload;
 
+    rf12_sleep(RF12_WAKEUP);
+    
     while (!rf12_canSend())
         rf12_recvDone();
     
     rf12_sendStart(0, &payload, sizeof payload);
+    // set the sync mode to 2 if the fuses are still the Arduino default
+    // mode 3 (full powerdown) can only be used with 258 CK startup fuses
+    rf12_sendWait(2);
     
-    MilliTimer t;
-    while (!t.poll(1000))
-        rf12_recvDone();
+    rf12_sleep(RF12_SLEEP);
+    Sleepy::loseSomeTime(60000);
 }
