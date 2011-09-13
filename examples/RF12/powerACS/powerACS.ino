5a4a1efc6c4ee4129ea35774bf261d492de68740
Jean-Claude Wippler <jc@wippler.nl>
2011-09-14 00:40:24 +0200

    add powerACS sketch

---
+++ examples/RF12/powerACS/powerACS.ino
@@ -0,0 +1,59 @@
+// Measure power from an ACS714.
+// 2011-09-13 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
+// $Id: $
+
+// ACS714 output pin is tied to AIO3. Power is 5V, so it swings around 2.5V.
+// Basic approach is to track a moving average, and to calculate the average
+// *absolute* difference from this value. Result is in microvolts.
+
+#include <JeeLib.h>
+#define DEBUG 1
+
+Port measure (3);
+MilliTimer report;
+word count;
+uint32_t total, avg = 2500000; // best guess for startup
+
+void setup () {
+#if DEBUG
+  Serial.begin(57600);
+  Serial.println("\n[powerACS]");
+#endif
+  rf12_initialize(17, RF12_868MHZ, 5);
+}
+
+void loop () {
+  // convert ADC reading to microvolts 0..3300000
+  uint32_t value = measure.anaRead() * (3300000L / 1023);
+  
+  // keep track of the (slow-) moving average
+  avg = (499L * avg + value) / 500;
+
+  // accumulate the absolute differences from the average
+  if (value > avg)
+    total += value - avg;
+  else if (value < avg)
+    total += avg - value;
+  ++count;
+  
+  if (report.poll(1000)) {
+    // the reported value is the average absolute value
+    uint32_t range = total / count;
+#if DEBUG    
+    Serial.print(value);
+    Serial.print(' ');
+    Serial.print(avg);
+    Serial.print(' ');
+    Serial.print(count);
+    Serial.print(' ');
+    Serial.print(total);
+    Serial.print(' ');
+    Serial.println(range);
+#else
+    while (!rf12_canSend())
+      rf12_recvDone();
+    rf12_sendStart(0, &range, sizeof range);
+#endif    
+    total = count = 0;
+  }
+}
