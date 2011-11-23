588cc85ccabc75f810c197bbd77cdc1cd60362f8
Jean-Claude Wippler <jc@wippler.nl>
2011-11-23 01:01:33 +0100

    add poller / pollee wireless speed example sketches

---
+++ examples/RF12/pollee/pollee.ino
@@ -0,0 +1,35 @@
+// This can run on several nodes, and get data to the central "poller" node.
+// 2011-11-23 <jcw@equi4.com> http://opensource.org/licenses/mit-license.php
+//
+// Warning: this test will flood the radio band so nothing else gets through!
+//
+// To prepare for this test, you need to upload RF12demo to each pollee first,
+// and set its node ID, group (77) and band (868 MHz). Node IDs must be in the 
+// range 1 .. NUM_NODES, as defined in poller.ino (all nodes must be present).
+
+#include <JeeLib.h>
+
+typedef struct {
+  byte node;
+  long time;
+} Payload;
+
+Payload payload;
+
+void setup () {
+  Serial.begin(57600);
+  Serial.print("\n[pollee] ");
+  // use the node ID previously stored in EEPROM by RF12demo
+  payload.node = rf12_config();
+  Serial.println((word) payload.node);
+}
+
+void loop () {
+  // wait for an incoming empty packet for us
+  if (rf12_recvDone() && rf12_crc == 0 && rf12_len == 0 && RF12_WANTS_ACK) {
+    // invent some data to send back
+    payload.time = millis();
+    // start transmission
+    rf12_sendStart(RF12_ACK_REPLY, &payload, sizeof payload);
+  }
+}
---
+++ examples/RF12/poller/poller.ino
@@ -0,0 +1,47 @@
+// Polls a number of "pollee" nodes as fast as possible to get data from them.
+// 2011-11-23 <jcw@equi4.com> http://opensource.org/licenses/mit-license.php
+//
+// Warning: this test will flood the radio band so nothing else gets through!
+//
+// Maximum rates will only be achieved if all nodes 1 .. NUM_NODES are powered
+// up, properly configured, and pre-loaded with the pollee.ino sketch.
+
+#include <JeeLib.h>
+
+const byte NUM_NODES = 1; // poll using node ID from 1 to NUM_NODES 
+
+typedef struct {
+  byte node;
+  long time;
+} Payload;
+
+byte nextId;
+MilliTimer timer;
+
+void setup () {
+  Serial.begin(57600);
+  Serial.println("\n[poller]");
+  rf12_initialize(1, RF12_868MHZ, 77);
+}
+
+void loop () {
+  // switch to next node
+  if (++nextId > NUM_NODES)
+    nextId = 1;
+  // wait until we can send a packet
+  while (!rf12_canSend())
+    rf12_recvDone();
+  // send an empty packet to one specific pollee
+  rf12_sendStart(nextId | RF12_HDR_ACK, 0, 0);
+  // wait up to 5 milliseconds for a reply
+  timer.set(5);
+  while (!timer.poll())
+    if (rf12_recvDone() && rf12_crc == 0 && rf12_len == sizeof (Payload)) {
+      // got a good ACK packet, print out its contents
+      const Payload* p = (const Payload*) rf12_data;
+      Serial.print(p->node);
+      Serial.print(": ");
+      Serial.println(p->time);
+      break;
+    }
+}
