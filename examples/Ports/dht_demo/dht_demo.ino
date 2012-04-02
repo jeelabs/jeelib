482376da13f6ff024f086ae3aa3ca2ebe3bc9d57
Jean-Claude Wippler <jc@wippler.nl>
2012-04-02 03:26:55 +0200

    add dht_demo sketch

---
+++ examples/Ports/dht_demo/dht_demo.ino
@@ -0,0 +1,22 @@
+// Demo readout of the DHT11 or DHT22 temperature/humidity sensors.
+// 2012-04-02 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
+
+#include <JeeLib.h>
+
+DHTxx dht (5); // connected to DIO2
+
+void setup () {
+  Serial.begin(57600);
+  Serial.println("\n[dht_demo]");
+}
+
+void loop () {
+  int t, h;
+  dht.reading(t, h);
+  Serial.print("temperature = ");
+  Serial.println(t);
+  Serial.print("humidity = ");
+  Serial.println(h);
+  Serial.println();
+  delay(3000);
+}
