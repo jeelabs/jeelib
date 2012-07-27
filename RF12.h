b14887aff218a4bc543295bd595a8c5eb08e6a81
Jasper Klein <vliegendehuiskat@gmail.com>
2012-07-27 21:35:37 +0200

    RF12.h documentation.

--- RF12.h
+++ RF12.h
@@ -1,13 +1,14 @@
-// RFM12B driver definitions
-// 2009-02-09 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
+/** RFM12B driver definitions
+ *  2009-02-09 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
+ */
 
 #ifndef RF12_h
 #define RF12_h
 
 #include <stdint.h>
 
-// version 1 did not include the group code in the crc
-// version 2 does include the group code in the crc
+/// version 1 did not include the group code in the crc
+/// version 2 does include the group code in the crc
 #define RF12_VERSION    2
 
 #define rf12_grp        rf12_buf[0]
@@ -15,11 +16,16 @@
 #define rf12_len        rf12_buf[2]
 #define rf12_data       (rf12_buf + 3)
 
+/// RF12 CTL bit mask.
 #define RF12_HDR_CTL    0x80
+/// RF12 DST bit mask.
 #define RF12_HDR_DST    0x40
+/// RF12 ACK bit mask.
 #define RF12_HDR_ACK    0x20
+/// RF12 HDR bit mask.
 #define RF12_HDR_MASK   0x1F
 
+/// RF12 Maximum message size in bytes.
 #define RF12_MAXDATA    66
 
 #define RF12_433MHZ     1
@@ -48,67 +54,68 @@ extern volatile uint8_t rf12_buf[];
 /// Seq number of encrypted packet (or -1)
 extern long rf12_seq;
 
-// only needed if you want to init the SPI bus before rf12_initialize does it
+/// Only needed if you want to init the SPI bus before rf12_initialize does it
 void rf12_spiInit(void);
 
-// call this once with the node ID, frequency band, and optional group
+/// Call this once with the node ID, frequency band, and optional group
 uint8_t rf12_initialize(uint8_t id, uint8_t band, uint8_t group=0xD4);
 
-// initialize the RF12 module from settings stored in EEPROM by "RF12demo"
-// don't call rf12_initialize() if you init the hardware with rf12_config()
-// returns the node ID as 1..31 value (1..26 correspond to nodes 'A'..'Z')
+/// Initialize the RF12 module from settings stored in EEPROM by "RF12demo"
+/// don't call rf12_initialize() if you init the hardware with rf12_config()
+/// @return the node ID as 1..31 value (1..26 correspond to nodes 'A'..'Z')
 uint8_t rf12_config(uint8_t show =1);
 
-// call this frequently, returns true if a packet has been received
+/// Call this frequently, returns true if a packet has been received
 uint8_t rf12_recvDone(void);
 
-// call this to check whether a new transmission can be started
-// returns true when a new transmission may be started with rf12_sendStart()
+/// call this to check whether a new transmission can be started
+/// @return true when a new transmission may be started with rf12_sendStart()
 uint8_t rf12_canSend(void);
 
-// call this only when rf12_recvDone() or rf12_canSend() return true
+/// Call this only when rf12_recvDone() or rf12_canSend() return true
 void rf12_sendStart(uint8_t hdr);
+/// Call this only when rf12_recvDone() or rf12_canSend() return true
 void rf12_sendStart(uint8_t hdr, const void* ptr, uint8_t len);
-// deprecated: use rf12_sendStart(hdr,ptr,len) followed by rf12_sendWait(sync)
+/// Deprecated: use rf12_sendStart(hdr,ptr,len) followed by rf12_sendWait(sync)
 void rf12_sendStart(uint8_t hdr, const void* ptr, uint8_t len, uint8_t sync);
 
-// wait for send to finish, sleep mode: 0=none, 1=idle, 2=standby, 3=powerdown
+/// Wait for send to finish. @param mode sleep mode 0=none, 1=idle, 2=standby, 3=powerdown
 void rf12_sendWait(uint8_t mode);
 
-// this simulates OOK by turning the transmitter on and off via SPI commands
-// use this only when the radio was initialized with a fake zero node ID
+/// This simulates OOK by turning the transmitter on and off via SPI commands.
+/// Use this only when the radio was initialized with a fake zero node ID.
 void rf12_onOff(uint8_t value);
 
-// power off the RF12, ms > 0 sets watchdog to wake up again after N * 32 ms
-// note: once off, calling this with -1 can be used to bring the RF12 back up
+/// power off the RF12, ms > 0 sets watchdog to wake up again after N * 32 ms
+/// note: once off, calling this with -1 can be used to bring the RF12 back up
 void rf12_sleep(char n);
 
-// returns true of the supply voltage is below 3.1V
+/// @return true if the supply voltage is below 3.1V
 char rf12_lowbat(void);
 
-// set up the easy tranmission mode, arg is number of seconds between packets
+/// Set up the easy tranmission mode, arg is number of seconds between packets
 void rf12_easyInit(uint8_t secs);
 
-// call this often to keep the easy transmission mode going
+/// Call this often to keep the easy transmission mode going
 char rf12_easyPoll(void);
 
-// send new data using the easy transmission mode, buffer gets copied to driver
+/// Send new data using the easy transmission mode, buffer gets copied to driver
 char rf12_easySend(const void* data, uint8_t size);
 
-// enable encryption (null arg disables it again)
+/// Enable encryption (null arg disables it again)
 void rf12_encrypt(const uint8_t*);
 
-// low-level control of the RFM12B via direct register access
-// http://tools.jeelabs.org/rfm12b is useful for calculating these
+/// Low-level control of the RFM12B via direct register access.
+/// http://tools.jeelabs.org/rfm12b is useful for calculating these.
 uint16_t rf12_control(uint16_t cmd);
 
-// See http://blog.strobotics.com.au/2009/07/27/rfm12-tutorial-part-3a/
-// Transmissions are packetized, don't assume you can sustain these speeds! 
-//
-// Note - data rates are approximate. For higher data rates you may need to
-// alter receiver radio bandwidth and transmitter modulator bandwidth.
-// Note that bit 7 is a prescaler - don't just interpolate rates between
-// RF12_DATA_RATE_3 and RF12_DATA_RATE_2.
+/// See http://blog.strobotics.com.au/2009/07/27/rfm12-tutorial-part-3a/
+/// Transmissions are packetized, don't assume you can sustain these speeds! 
+///
+/// Note - data rates are approximate. For higher data rates you may need to
+/// alter receiver radio bandwidth and transmitter modulator bandwidth.
+/// Note that bit 7 is a prescaler - don't just interpolate rates between
+/// RF12_DATA_RATE_3 and RF12_DATA_RATE_2.
 enum rf12DataRates {
     RF12_DATA_RATE_CMD = 0xC600,
     RF12_DATA_RATE_9 = RF12_DATA_RATE_CMD | 0x02,  // Approx 115200 bps
