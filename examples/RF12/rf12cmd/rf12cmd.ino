cd590a0da065d3634da1ad9defea90ddfd717dbd
Jean-Claude Wippler <jc@wippler.nl>
2012-10-21 00:28:29 +0200

    add homePower sample sketch

--- examples/RF12/rf12cmd/rf12cmd.ino
+++ examples/RF12/rf12cmd/rf12cmd.ino
@@ -17,6 +17,10 @@ void EmBencode::PushChar (char ch) {
   Serial.write(ch);
 }
 
+static uint8_t freqCode (int freq) {
+  return freq == 433 ? RF12_433MHZ : freq == 868 ? RF12_868MHZ : RF12_915MHZ;
+}
+
 static void pushGreeting () {
   encoder.startList();
   encoder.push("hi");
@@ -27,6 +31,7 @@ static void pushGreeting () {
 }
 
 static void pushBytes (uint8_t hdr, const void* ptr, uint8_t len) {
+  Serial.flush();
   encoder.startList();
   encoder.push("rx");
   encoder.push(BAND);
@@ -36,16 +41,69 @@ static void pushBytes (uint8_t hdr, const void* ptr, uint8_t len) {
   encoder.endList();
 }
 
-void setup () {
-    Serial.begin(115200);
-    pushGreeting();
+static void pushError (const char* tag) {
+  Serial.flush();
+  encoder.startList();
+  encoder.push(1);
+  encoder.push("c?");
+  if (tag != 0)
+    encoder.push(tag);
+  encoder.endList();
+}
 
-    uint8_t band = BAND == 433 ? RF12_433MHZ :
-                    BAND == 868 ? RF12_868MHZ : RF12_915MHZ;
-    rf12_initialize(1, band, GROUP);
+static bool do_tx () {
+  if (decoder.nextToken() != EmBdecode::T_NUMBER) return false;
+  int band = decoder.asNumber();
+  if (decoder.nextToken() != EmBdecode::T_NUMBER) return false;
+  uint8_t group = decoder.asNumber();
+  if (decoder.nextToken() != EmBdecode::T_NUMBER) return false;
+  char node = decoder.asNumber();
+  if (decoder.nextToken() != EmBdecode::T_STRING) return false;
+
+  rf12_initialize(node < 0 ? -node : 1, freqCode(band), group);
+
+  // this may lose packets, but we're probably in the wrong band/group anyway
+  while (!rf12_canSend())
+    rf12_recvDone();
+
+  // send the packet out on the specified frequency band and netgroup
+  uint8_t len;
+  const void* data = decoder.asString(&len);
+  // rf12_sendStart(node < 0 ? 0 : node, data, len);
+  rf12_sendStart(1, "\3", 1);
+  rf12_sendWait(1);
+
+  rf12_initialize(1, freqCode(BAND), GROUP);
+  return true;
+}
+
+static void dispatch () {
+  bool ok = false;
+  const char* cmd = 0;
+
+  if (decoder.nextToken() == EmBdecode::T_LIST &&
+      decoder.nextToken() == EmBdecode::T_STRING) {
+    cmd = decoder.asString();
+    ok = strcmp(cmd, "tx") == 0 && do_tx();
+  }
+  
+  decoder.reset();
+  if (!ok)
+    pushError(cmd);
+}
+
+void setup () {
+  Serial.begin(115200);
+  pushGreeting();
+  while (Serial.read() >= 0)
+    ;
+  rf12_initialize(1, freqCode(BAND), GROUP);
 }
 
 void loop () {
   if (rf12_recvDone() && rf12_crc == 0)
     pushBytes(rf12_hdr, (const void*) rf12_data, rf12_len);
+
+  if (Serial.available() && decoder.process(Serial.read()) > 0)
+    dispatch();
 }
