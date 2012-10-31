1536adb18991f2370c4014a4312502c984763c76
Jean-Claude Wippler <jc@wippler.nl>
2012-10-31 13:14:31 +0100

    predict next packet times

--- examples/RF12/syncRecv/syncRecv.ino
+++ examples/RF12/syncRecv/syncRecv.ino
@@ -4,68 +4,112 @@
 // 2012-10-30 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
 
 #include <JeeLib.h>
+#include <avr/eeprom.h>
 
-#define SEND_ID 9       // wait for packets from node 9
+#define SEND_FREQ   RF12_868MHZ   // listening frequency
+#define SEND_GROUP  5             // listening net group
+#define SEND_ID     9             // listen for this node ID
 
-word estimate = 3000;   // packet expected every 3s
-word window = 512;      // start with +/- 17 % window, power of 2
-byte missed;            // count how often in a row receive failed
+#define CYCLE_TIME  3000          // expected cycle, milliseconds
+
+#define EEADDR ((word*) 0)        // save estimate at this EEPROM address
+
+word estimate;      // packet expected every 3s
+word window;        // start with +/- 17 % window, power of 2
+byte missed;        // count how often in a row receive failed
+uint32_t lastRecv;  // time of last reception
 
 ISR(WDT_vect) { Sleepy::watchdogEvent(); }
+
+static void radioSleep (word ms) {
+  rf12_sleep(RF12_SLEEP);
+  Sleepy::loseSomeTime(ms);
+  rf12_sleep(RF12_WAKEUP);
+}
 
 static bool isDesiredPacket () {
   return rf12_recvDone() && rf12_crc == 0 && rf12_hdr == SEND_ID;
 }
 
-// since the watchdog timer is inaccurate, we must estimate
 static bool estimateWithWatchdog () {
   // first wait indefinitely for a packet to come in
   while (!isDesiredPacket())
     ;
-  Serial.print("R");
-  delayMicroseconds(250);
 
   // sleep until estimate - window
   word lowEstimate = estimate - window;
   uint32_t start = millis();
-  rf12_sleep(RF12_SLEEP);
-  Sleepy::loseSomeTime(lowEstimate);
-  rf12_sleep(RF12_WAKEUP);
+  radioSleep(lowEstimate);
 
   // using the millis() timer, measure when the next packet comes
-  word diff;
   do {
-    diff = millis() - start;
-    if (diff > estimate + window)
+    lastRecv = millis();
+    if (lastRecv - start > estimate + window)
       return false;
   } while (!isDesiredPacket());
 
-  int offset = window - (diff - lowEstimate);
-  // the new estimate shouldn't be too close to the exact time
-  estimate = (diff + 2) & ~0x0F; // truncate down to 16 ms multiple
+  estimate = lastRecv - start;
+  int offset = (estimate - lowEstimate) - window;
 
-  Serial.print(" ok at "); Serial.print(diff);
+  Serial.print(" got "); Serial.print(estimate);
   Serial.print(", window = "); Serial.print(window);
-  Serial.print(", offset = "); Serial.print(offset);
-  Serial.print(", new estimate = "); Serial.println(estimate);
+  Serial.print(", offset = "); Serial.println(offset);
   return true;
+}
+
+static void chooseEstimate () {
+  // use estimate in EEPROM, if it's sensible
+  estimate = eeprom_read_word(EEADDR);
+  if (estimate < CYCLE_TIME - CYCLE_TIME/5 ||
+      estimate > CYCLE_TIME + CYCLE_TIME/5)
+    estimate = CYCLE_TIME;
+  Serial.print("start estimate = "); Serial.println(estimate);
+  // narrow down estimate from 20 % -> 2 %
+  window = estimate / 5; 
+  while (window >= 16)
+    if (estimateWithWatchdog())
+      window /= 2;
+  // save best estimaet so far
+  eeprom_write_word(EEADDR, estimate);
+  Serial.print("save estimate "); Serial.println(estimate);
 }
 
 void setup () {
   Serial.begin(57600);
   Serial.println("\n[syncRecv]");
-  rf12_initialize(1, RF12_868MHZ, 5); // never sends anything
+  rf12_initialize(1, SEND_FREQ, SEND_GROUP); // we never send
+  chooseEstimate();
 }
 
 void loop () {
-  if (estimateWithWatchdog()) {
-    // gradually narrow down the window whenever reception succeeds
-    if (window >= 32)
-      window /= 2;
-    missed = 0;
-  } else {
-    // but widen the window again if reception fails for too long
-    if (window <= 512 && ++missed % 10 == 0)
-      window *= 4; // increase window x4 at every 10 packets missed
+  uint32_t now = millis();
+  byte lost = (now - lastRecv + estimate/2) / estimate;
+  Serial.print(" #"); Serial.print(lost);
+  if (lost > 20)
+    chooseEstimate();
+  else {
+    uint32_t predict = lastRecv + (lost + 1) * estimate;
+    word sleep = predict - now - window;
+
+    Serial.print(" s "); Serial.print(sleep);
+    Serial.flush(); delayMicroseconds(250);
+
+    radioSleep(sleep);
+
+    uint32_t recvTime;
+    do {
+      recvTime = millis();
+      if (recvTime > predict + window)
+        return;
+    } while (!isDesiredPacket());
+
+    estimate = (recvTime - lastRecv) / (lost + 1);
+    int offset = (estimate - sleep) - window;
+    lastRecv = recvTime;
+
+    Serial.print(" e "); Serial.print(estimate);
+    Serial.print(" w "); Serial.print(window);
+    Serial.print(" o "); Serial.print(offset);
+    Serial.print(" rf "); Serial.println(offset + window);
   }
 }
