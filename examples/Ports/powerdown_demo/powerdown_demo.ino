e45a5012270a01f8d5b9b26762cb2960c318af73
Jean-Claude Wippler <jc@wippler.nl>
2013-05-06 14:10:47 +0200

    blech, it was still not right

--- examples/Ports/powerdown_demo/powerdown_demo.ino
+++ examples/Ports/powerdown_demo/powerdown_demo.ino
@@ -1,31 +1,15 @@
 /// @dir powerdown_demo
-/// Sample code to power down a JeeNode completely.
+/// Sample code to completely power down a JeeNode (std or micro).
 // 2010-08-16 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
 
 #include <JeeLib.h>
 
 void setup () {
-    // get the pre-scaler into a known state
-    cli();
-    CLKPR = bit(CLKPCE);
-#if defined(__AVR_ATtiny84__)
-    CLKPR = 0; // div 1, i.e. speed up to 8 MHz
-#else
-    CLKPR = 1; // div 2, i.e. slow down to 8 MHz
-#endif
-    sei();
-
-#if defined(__AVR_ATtiny84__)
-    // power up the radio on JMv3
-    bitSet(DDRB, 0);
-    bitClear(PORTB, 0);
-#endif
-
-    // turn the radio off completely
+#if !defined(__AVR_ATtiny84__)
+    // turn the radio off (the JMv3 already powers up with MOSFET off)
     rf12_initialize(17, RF12_868MHZ);
     rf12_sleep(RF12_SLEEP);
 
-#if !defined(__AVR_ATtiny84__)
     // blink the blue LED three times, just because we can
     PORTB |= bit(1);
     DDRB |= bit(1);
