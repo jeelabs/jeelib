ec0d9b0fb20044f46b4dd11f38df4ef4b60c1113
Jean-Claude Wippler <jc@wippler.nl>
2013-05-24 23:20:19 +0200

    add simple ChibiOS example sketch

---
+++ examples/RTOS/chiBlink/chiBlink.ino
@@ -0,0 +1,35 @@
+/// @dir chiBlink
+/// Simple LED blinker using the ChibiOS real-time operating system
+/// @see http://jeelabs.org/2013/05/24/blinking-in-real-time/
+// 2013-05-23 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
+
+#include <ChibiOS_AVR.h>
+
+static WORKING_AREA(waThread1, 50);
+
+void Thread1 (void* arg) {
+  const uint8_t LED_PIN = 9;
+  pinMode(LED_PIN, OUTPUT);
+  
+  while (1) {
+    digitalWrite(LED_PIN, LOW);
+    chThdSleepMilliseconds(100);
+    digitalWrite(LED_PIN, HIGH);
+    chThdSleepMilliseconds(500);
+  }
+}
+
+void setup () {
+  chBegin(mainThread);
+}
+
+void mainThread () {
+  chThdCreateStatic(waThread1, sizeof (waThread1),
+                    NORMALPRIO + 2, (tfunc_t) Thread1, 0);
+  while (true)
+    loop();
+}
+
+void loop () {
+  delay(1000);
+}
