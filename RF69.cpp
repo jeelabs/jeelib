345f2aacb7cb7ebd6f7e863bb4af46ab7214cdbb
John O'Hare <john@o-hare.net>
2014-06-01 12:23:44 +0100

    More plausible, clear the AFC at initialisation
    If 0x29 is above the noise floor then the AFC is not cleared ready for
    the first packet. If below noise floor then noise gets things rolling.
    My best guess so far.

--- RF69.cpp
+++ RF69.cpp
@@ -88,9 +88,9 @@ static ROM_UINT8 configRegs_compat [] ROM_DATA = {
 // 0x11, 0x5F, // PA1 enable, Pout = max // uncomment this for RFM69H  0x19, 0x42, // RxBw ...
 */
   0x1A, 0x91, // 0x8B,   // Channel filter BW
-  0x1E, 0x0C, // AfcAutoclearOn, AfcAutoOn
+  0x1E, 0x0E, // AfcAutoclearOn, AfcAutoOn
   0x25, 0x80, // DioMapping1 = SyncAddress (Rx)
-  0x29, 0xC8, // RssiThresh ...
+  0x29, 0xC4, // RssiThresh ...
 
   0x2E, 0xA0, // SyncConfig = sync on, sync size = 5
   0x2F, 0xAA, // SyncValue1 = 0xAA
