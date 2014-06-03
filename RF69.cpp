71771e805a23ba2a07e70c18dae2691c27795283
John O'Hare <john@o-hare.net>
2014-06-03 20:53:46 +0100

    Tidy up

--- RF69.cpp
+++ RF69.cpp
@@ -82,11 +82,11 @@ static ROM_UINT8 configRegs_compat [] ROM_DATA = {
   // 0x08, 0x00, // FrfMib, divider = 14221312
   // 0x09, 0x00, // FrfLsb, step = 61.03515625
   0x0B, 0x20, // AfcCtrl, afclowbetaon
-  0x19, 0x42, // RxBw ...
 /*
 // Mismatching PA1 below with the RFM69x module present risks blowing a hole in the LNA
 // 0x11, 0x5F, // PA1 enable, Pout = max // uncomment this for RFM69H  0x19, 0x42, // RxBw ...
 */
+  0x19, 0x42, // RxBw ...
   0x1A, 0x91, // 0x8B,   // Channel filter BW
   0x1E, 0x0E, // AfcAutoclearOn, AfcAutoOn
   0x25, 0x80, // DioMapping1 = SyncAddress (Rx)
