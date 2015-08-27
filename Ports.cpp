b8a5b2660787f5b7cd501e7fc83f74ee52b38ee1
John O'Hare <john@o-hare.net>
2015-08-27 15:55:41 +0100

    Bug fix

--- Ports.cpp
+++ Ports.cpp
@@ -1259,8 +1259,8 @@ word Sleepy::idleTimer (byte prescale) {
     if (millisAdjust == 2) break;
     if (!(++timeIdle + 1)) break;     // Might detect just before overflow
     }
+    PRR = savePRR;                // re-enable what we disabled
     TIMSK0 = saveTIMSK0;          // re-enable what we disabled
-    PRR = savePRR;                //
     return timeIdle;              // Complete intervals
 }
 
