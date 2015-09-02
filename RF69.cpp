8dc504f3f3f0698aa3ea180cea847e0fbb0f397d
John O'Hare <john@o-hare.net>
2015-09-02 17:49:32 +0100

    Bug crept in from somewhere

--- RF69.cpp
+++ RF69.cpp
@@ -151,8 +151,8 @@ static ROM_UINT8 configRegs_compat [] ROM_DATA = {
  // 0x02, 0x00, // DataModul = packet mode, fsk
   0x03, 0x02, // BitRateMsb, data rate = 49,261 khz
   0x04, 0x8A, // BitRateLsb, divider = 32 MHz / 650 == 49,230 khz
-  0x05, 0x02, // FdevMsb = 45 KHz
-  0x06, 0xE1, // FdevLsb = 45 KHz
+  0x05, 0x05, // FdevMsb = 90 KHz
+  0x06, 0xC3, // FdevLsb = 90 KHz
   // 0x07, 0xD9, // FrfMsb, freq = 868.000 MHz
   // 0x08, 0x00, // FrfMib, divider = 14221312
   // 0x09, 0x00, // FrfLsb, step = 61.03515625
