54e49bdbdff57779d25dbdffaedaf33e014d11ba
John O'Hare <john@o-hare.net>
2015-12-01 16:52:56 +0000

    Set defaults to RFM69

--- RF12.cpp
+++ RF12.cpp
@@ -14,7 +14,7 @@
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
-#define RF69_COMPAT 0    // Set this true to use the RF69 driver
+#define RF69_COMPAT 1    // Set this true to use the RF69 driver
 #define PINCHG_IRQ  0    // Set this true to use pin-change interrupts
                          // The above flags must be set similarly in RF69_avr.h
 
