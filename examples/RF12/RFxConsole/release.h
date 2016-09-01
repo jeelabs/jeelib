af173f4756623c310dcb57ccb47d41db9c4c4a2a
John O'Hare <john@o-hare.net>
2016-09-01 10:51:34 +0100

    Add files via upload
    Untracked version file

---
+++ examples/RF12/RFxConsole/release.h
@@ -0,0 +1,10 @@
+// [RFxConsole] version 0.1.14 SHA1 41dc6ed8a0b8a87b75a71f1845db3f0548b7238f
+
+// You may change the following lines to suit your needs
+#define CODE_NAME "RFxConsole-RSSI"
+#define VERSION_MAJOR "2.x"
+
+//The following lines may be changed automatically
+#define VERSION "\n[RFxConsole.2]"
+#define VERSION_SHA1 "c057b5f4c072229fccff8e4359717ee2f86989e7"
+#define VERSION_SIG  "c057b5f"
