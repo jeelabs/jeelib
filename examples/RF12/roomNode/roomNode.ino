bd4ac34a82c61fbe6ce64fe88b7d59c1a90401c1
John O'Hare <john@o-hare.net>
2017-06-02 16:26:05 +0100

    Freshen up Airwick Room Node
    Clear up stray PIR interrupts. Add remote control of timings using
    extended ACK.

--- examples/RF12/roomNode/roomNode.ino
+++ examples/RF12/roomNode/roomNode.ino
@@ -1,4 +1,5 @@
 /// @dir roomNode
+/// A version of Room Node using Airwick PIR with a couple of tweaks. JOH 2017/06/02
 /// New version of the Room Node (derived from rooms.pde).
 // 2010-10-19 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
 
@@ -13,21 +14,26 @@
 #include <PortsSHT11.h>
 #include <avr/sleep.h>
 #include <util/atomic.h>
+#include <avr/eeprom.h>
+#include <util/crc16.h>
+#define crc_update      _crc16_update
 
 #define SERIAL  0   // set to 1 to also report readings on the serial port
 #define DEBUG   0   // set to 1 to display each loop() run and PIR trigger
 
-// #define SHT11_PORT  1   // defined if SHT11 is connected to a port
-#define HYT131_PORT 1   // defined if HYT131 is connected to a port
+#define SHT11_PORT  1   // defined if SHT11 is connected to a port
+//	#define HYT131_PORT 1   // defined if HYT131 is connected to a port
 #define LDR_PORT    4   // defined if LDR is connected to a port's AIO pin
 #define PIR_PORT    4   // defined if PIR is connected to a port's DIO pin
 
-#define MEASURE_PERIOD  600 // how often to measure, in tenths of seconds
+//#define MEASURE_PERIOD  600 // how often to measure, in tenths of seconds
 #define RETRY_PERIOD    10  // how soon to retry if ACK didn't come in
 #define RETRY_LIMIT     5   // maximum number of times to retry
 #define ACK_TIME        10  // number of milliseconds to wait for an ack
-#define REPORT_EVERY    5   // report every N measurement cycles
+//#define REPORT_EVERY    60  // report every N measurement cycles
 #define SMOOTH          3   // smoothing factor used for running averages
+
+#define SETTINGS_EEPROM_ADDR ((uint8_t*) 0x00)
 
 // set the sync mode to 2 if the fuses are still the Arduino default
 // mode 3 (full powerdown) can only be used with 258 CK startup fuses
@@ -48,12 +54,26 @@ static byte myNodeID;       // node ID used for this unit
 // This defines the structure of the packets which get sent out by wireless:
 
 struct {
-    byte light;     // light sensor: 0..255
-    byte moved :1;  // motion detector: 0..1
-    byte humi  :7;  // humidity: 0..100
-    int temp   :10; // temperature: -500..+500 (tenths)
-    byte lobat :1;  // supply voltage dropped under 3.1V: 0..1
+	byte command;		// ACK command return field
+    byte light;     	// light sensor: 0..255
+    byte moved :1;  	// motion detector: 0..1
+    byte humi  :7;  	// humidity: 0..100
+    int temp   :10; 	// temperature: -500..+500 (tenths)
+    byte lobat :1;  	// supply voltage dropped under 3.1V: 0..1
+    unsigned int count;	// Count of PCINT's
+    byte attempts	:4;	// PIR transmission attempts
+    byte missedACK	:4;	// 
 } payload;
+
+typedef struct {
+    byte start;
+    uint16_t MEASURE_PERIOD;
+    uint16_t REPORT_EVERY;
+    uint8_t MEASURE;
+    uint8_t REPORT;
+    word crc;
+} eeprom;
+static eeprom settings;
 
 // Conditional code, depending on which sensors are connected and how:
 
@@ -72,7 +92,7 @@ struct {
 
 #if PIR_PORT
     #define PIR_HOLD_TIME   30  // hold PIR value this many seconds after change
-    #define PIR_PULLUP      1   // set to one to pull-up the PIR input pin
+    #define PIR_PULLUP      0   // set to one to pull-up the PIR input pin
     #define PIR_INVERTED    1   // 0 or 1, to match PIR reporting high or low
     
     /// Interface to a Passive Infrared motion sensor.
@@ -116,9 +136,16 @@ struct {
     };
 
     PIR pir (PIR_PORT);
-
+    
+	volatile bool maskPCINT = true;
+	volatile unsigned int countPCINT;
     // the PIR signal comes in via a pin-change interrupt
-    ISR(PCINT2_vect) { pir.poll(); }
+    ISR(PCINT2_vect) {
+    	countPCINT++; 
+    	if (!maskPCINT) {
+    		pir.poll(); 
+    	}
+    }
 #endif
 
 // has to be defined because we're using the watchdog for low-power waiting
@@ -136,20 +163,6 @@ static void shtDelay () {
     Sleepy::loseSomeTime(32); // must wait at least 20 ms
 }
 
-// wait a few milliseconds for proper ACK to me, return true if indeed received
-static byte waitForAck() {
-    MilliTimer ackTimer;
-    while (!ackTimer.poll(ACK_TIME)) {
-        if (rf12_recvDone() && rf12_crc == 0 &&
-                // see http://talk.jeelabs.net/topic/811#post-4712
-                rf12_hdr == (RF12_HDR_DST | RF12_HDR_CTL | myNodeID))
-            return 1;
-        set_sleep_mode(SLEEP_MODE_IDLE);
-        sleep_mode();
-    }
-    return 0;
-}
-
 // readout all the sensors and other values
 static void doMeasure() {
     byte firstTime = payload.humi == 0; // special case to init running avg
@@ -162,7 +175,8 @@ static void doMeasure() {
         sht11.measure(SHT11::TEMP, shtDelay);
         float h, t;
         sht11.calculate(h, t);
-        int humi = h + 0.5, temp = 10 * t + 0.5;
+//        int humi = h + 0.5, temp = 10 * t + 0.5;
+        int humi = h, temp = 10 * t;
 #else
         //XXX TINY!
         int humi = 50, temp = 25;
@@ -196,13 +210,14 @@ static void serialFlush () {
 
 // periodic report, i.e. send out a packet and optionally report on serial port
 static void doReport() {
+//	payload.count = countPCINT;
     rf12_sleep(RF12_WAKEUP);
-    rf12_sendNow(0, &payload, sizeof payload);
+    rf12_sendNow(0, &payload, ((sizeof payload) - 3));
     rf12_sendWait(RADIO_SYNC_MODE);
     rf12_sleep(RF12_SLEEP);
 
     #if SERIAL
-        Serial.print("ROOM ");
+        Serial.print("\nROOM ");
         Serial.print((int) payload.light);
         Serial.print(' ');
         Serial.print((int) payload.moved);
@@ -212,6 +227,8 @@ static void doReport() {
         Serial.print((int) payload.temp);
         Serial.print(' ');
         Serial.print((int) payload.lobat);
+        Serial.print(' ');
+        Serial.print((int) countPCINT);
         Serial.println();
         serialFlush();
     #endif
@@ -220,12 +237,16 @@ static void doReport() {
 // send packet and wait for ack when there is a motion trigger
 static void doTrigger() {
     #if DEBUG
-        Serial.print("PIR ");
+        Serial.print("\nPIR ");
         Serial.print((int) payload.moved);
+        Serial.print(' ');
+        Serial.print((int) countPCINT);
         serialFlush();
     #endif
-
+    
+	payload.count = countPCINT;
     for (byte i = 0; i < RETRY_LIMIT; ++i) {
+    	payload.attempts = i;
         rf12_sleep(RF12_WAKEUP);
         rf12_sendNow(RF12_HDR_ACK, &payload, sizeof payload);
         rf12_sendWait(RADIO_SYNC_MODE);
@@ -233,23 +254,71 @@ static void doTrigger() {
         rf12_sleep(RF12_SLEEP);
 
         if (acked) {
-            #if DEBUG
-                Serial.print(" ack ");
-                Serial.println((int) i);
-                serialFlush();
-            #endif
+        #if DEBUG
+            Serial.print(" ack ");
+            Serial.println((int) i);
+            serialFlush();
+        #endif
+
+	        if (rf12_buf[2] > 0) {                  // Non-zero length ACK packet?
+	            payload.command = rf12_buf[3];		// Acknowledge the command
+
+				switch (rf12_buf[3]) {
+					case 99:
+						//Serial.println("Saving settings to eeprom");
+                      	saveSettings();
+                      	break;      
+					case 100:
+						settings.MEASURE = false;
+                  		break;
+					case 200:
+						settings.REPORT = false;
+                  		break;
+
+                      	if (rf12_buf[3] > 0 && rf12_buf[3] < 10) {
+	                          break;
+                      	}
+                      	if (rf12_buf[3] > 100 && rf12_buf[3] < 200) {
+                      		settings.MEASURE_PERIOD = ((rf12_buf[3]) - 100) * 10;
+                      		settings.MEASURE = true;
+                          	break;  
+                      	}
+                      	if (rf12_buf[3] > 200 && rf12_buf[3] <= 255) {
+                      		settings.REPORT_EVERY = ((rf12_buf[3]) - 200) * 10;
+                      		settings.REPORT = true;
+                          	break;
+                      	}
+                       // Serial.println("Unknown Command");
+                     	break;
+                  	} // end switch
+          	}
             // reset scheduling to start a fresh measurement cycle
-            scheduler.timer(MEASURE, MEASURE_PERIOD);
+            scheduler.timer(MEASURE, settings.MEASURE_PERIOD);
             return;
         }
         
+    	payload.missedACK++;
         delay(RETRY_PERIOD * 100);
     }
-    scheduler.timer(MEASURE, MEASURE_PERIOD);
+    scheduler.timer(MEASURE, settings.MEASURE_PERIOD);
     #if DEBUG
         Serial.println(" no ack!");
         serialFlush();
     #endif
+}
+
+// wait a few milliseconds for proper ACK to me, return true if indeed received
+static byte waitForAck() {
+    MilliTimer ackTimer;
+    while (!ackTimer.poll(ACK_TIME)) {
+        if (rf12_recvDone() && rf12_crc == 0 &&
+                // see http://talk.jeelabs.net/topic/811#post-4712
+                rf12_hdr == (RF12_HDR_DST | RF12_HDR_CTL | myNodeID))
+            return 1;
+        set_sleep_mode(SLEEP_MODE_IDLE);
+        sleep_mode();
+    }
+    return 0;
 }
 
 void blink (byte pin) {
@@ -259,15 +328,57 @@ void blink (byte pin) {
     }
 }
 
+static void saveSettings () {
+    settings.start = ~0;
+    settings.crc = calcCrc(&settings, sizeof settings - 2);
+    // this uses 170 bytes less flash than eeprom_write_block(), no idea why
+    for (byte i = 0; i < sizeof settings; ++i) {
+        byte* p = &settings.start;
+        if (eeprom_read_byte(SETTINGS_EEPROM_ADDR + i) != p[i]) {
+            eeprom_write_byte(SETTINGS_EEPROM_ADDR + i, p[i]);
+        }
+    }
+} // saveSettings
+
+static word calcCrc (const void* ptr, byte len) {
+    word crc = ~0;
+    for (byte i = 0; i < len; ++i)
+        crc = _crc16_update(crc, ((const byte*) ptr)[i]);
+    return crc;
+}
+
+static void loadSettings () {
+    uint16_t crc = ~0;
+    // eeprom_read_block(&settings, SETTINGS_EEPROM_ADDR, sizeof settings);
+    // this uses 166 bytes less flash than eeprom_read_block(), no idea why
+    for (byte i = 0; i < sizeof settings; ++i) {
+        ((byte*) &settings)[i] = eeprom_read_byte(SETTINGS_EEPROM_ADDR + i);
+        crc = crc_update(crc, ((byte*) &settings)[i]);
+    }
+     //Serial.print("Settings CRC ");
+    if (crc) {
+         //Serial.println("is bad, defaulting");
+         //Serial.println(crc, HEX);
+        settings.MEASURE_PERIOD = 540;
+        settings.REPORT_EVERY = 60;
+        settings.MEASURE = true;
+        settings.REPORT = true;
+    } else {
+         //Serial.println("is good");
+    }
+} // loadSettings
+
 void setup () {
     #if SERIAL || DEBUG
-        Serial.begin(57600);
-        Serial.print("\n[roomNode.3]");
+        Serial.begin(115200);
+        Serial.print("\n[roomNode.3.1]");
         myNodeID = rf12_config();
         serialFlush();
     #else
         myNodeID = rf12_config(0); // don't report info on the serial port
     #endif
+    
+	loadSettings();
     
     rf12_sleep(RF12_SLEEP); // power down
     
@@ -281,10 +392,13 @@ void setup () {
 #endif
     #endif
 
-    reportCount = REPORT_EVERY;     // report right away for easy debugging
+    reportCount = settings.REPORT_EVERY;     // report right away for easy debugging
     scheduler.timer(MEASURE, 0);    // start the measurement loop going
 }
 
+byte lastLight;
+byte lastHumi;
+int lastTemp;
 void loop () {
     #if DEBUG
         Serial.print('.');
@@ -293,8 +407,12 @@ void loop () {
 
     #if PIR_PORT
         if (pir.triggered()) {
+    		maskPCINT = true;	// Airwick PIR is skittish
             payload.moved = pir.state();
-            doTrigger();
+			doTrigger();
+			delay(5);
+			pir.triggered();	// Cleared any queued triggers
+    		maskPCINT = false;
         }
     #endif
     
@@ -302,19 +420,36 @@ void loop () {
 
         case MEASURE:
             // reschedule these measurements periodically
-            scheduler.timer(MEASURE, MEASURE_PERIOD);
+            scheduler.timer(MEASURE, settings.MEASURE_PERIOD);
     
-            doMeasure();
+            if (settings.MEASURE) doMeasure();
 
             // every so often, a report needs to be sent out
-            if (++reportCount >= REPORT_EVERY) {
-                reportCount = 0;
-                scheduler.timer(REPORT, 0);
+            if (settings.REPORT) {
+	            if ((++reportCount >= settings.REPORT_EVERY) 
+	             || (payload.temp != lastTemp)
+	             || (payload.humi != lastHumi)
+	             || (payload.light != lastLight) 
+	            ){
+	                reportCount = 0;
+	                scheduler.timer(REPORT, 0);
+	                lastLight = payload.light;
+	                lastTemp = payload.temp;
+	                lastHumi = payload.humi;
+	            }
             }
             break;
             
         case REPORT:
+    	#if PIR_PORT
+    		maskPCINT = true;	// Airwick PIR is skittish
+		#endif
+		
             doReport();
+
+    	#if PIR_PORT
+    		maskPCINT = false;
+        #endif
             break;
     }
 }
