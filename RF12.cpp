50ee4d95c4eca4427c7ae0645028742465ce2628
John O'Hare <john@o-hare.net>
2025-04-21 15:27:25 +0100

    Tweak rf12_leader

--- RF12.cpp
+++ RF12.cpp
@@ -17,7 +17,7 @@ extern rfAPI rfapi;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-#define RF69_COMPAT 0	// Set this true to use the RF69 driver
+#define RF69_COMPAT 1	// Set this true to use the RF69 driver
 #define PINCHG_IRQ  0	// Set this true to use pin-change interrupts
 						// The above flags must be set similarly in RF69_avr.h
 //#define Serial Serial1
@@ -571,7 +571,7 @@ void rf12_skip_hdr (uint8_t skip) {
 }
 uint8_t rf12_leader (uint8_t value) {
 	uint8_t v = rf12_lead;
-    rf12_lead = value;
+	if (value)rf12_lead = value;
     return v;
 }
 
--- examples/RF12/RFxConsole/RFxConsole.ino
+++ examples/RF12/RFxConsole/RFxConsole.ino
@@ -1,6 +1,6 @@
 /// @dir RFxConsole
 ///////////////////////////////////////////////////////////////////////////////
-#define RF69_COMPAT     0	// define this to use the RF69 driver i.s.o. RF12 
+#define RF69_COMPAT     1	// define this to use the RF69 driver i.s.o. RF12 
 ///							// The above flag must be set similarly in RF12.cpp
 ///							// and RF69_avr.h
 #define SX1276			0	// Also see setting in RF69.cpp & RF69_avr.h
@@ -860,7 +860,7 @@ static void showStatus() {
 #endif
     showString(PSTR(", DupTime "));
     Serial.print(DUPTIME);
-    showString(PSTR("s Framing 0x"));
+    showString(PSTR("s, Framing 0x"));
 	Serial.print(RF12_FRAME, HEX);
 	
 	if (watchNode) {
